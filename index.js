fcdb5fd4a088397e7352a8de0030ae65261d0f4f
Jon Taylor <jont@cakesolutions.net>
2017-06-10 18:17:30 +0100

    Basic demo working in Node.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const Parallel = require('paralleljs');
 
-const xs1 = Array.from(Array(100).keys());
-const xs2 = Array.from(Array(100).keys()).map(x => x + 100);
-const xs3 = Array.from(Array(100).keys()).map(x => x + 200).reverse();
-const xs = [].concat(xs1, xs2, xs3);
-console.log(`xs: ${xs}`);
+const NUM_ARRAYS = 256;
+const ARRAY_LENGTH = 256;
 
-const ys1 = xs.slice(0, 75);
-const ys2 = xs.slice(75, 150);
-const ys3 = xs.slice(150, 225);
-const ys4 = xs.slice(225, 300);
-console.log(`ys1: ${ys1}`);
-console.log(`ys2: ${ys2}`);
-console.log(`ys3: ${ys3}`);
-console.log(`ys4: ${ys4}`);
+const range = (start, len) => Array.from(Array(len).keys()).map(n => n + start);
+
+const xs = range(0, NUM_ARRAYS).map((_, index) => range(index * ARRAY_LENGTH, ARRAY_LENGTH)).reverse();
+
+function max(a, b) { return Math.max(a, b); }
+function reduceToMax(xs) { return xs.reduce(max, 0); }
+
+const opts = {
+    maxWorkers: 8
+};
+
+new Parallel(xs, opts)
+    .require(max)
+    .require(reduceToMax)
+    .map(data => reduceToMax(data))
+    .reduce(data => reduceToMax(data))
+    .then(data => console.log(`then: ${data}`));
